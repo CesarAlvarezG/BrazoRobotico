0c5a8e08d26280d09fc31ede6b0e33e0813de915
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-05-28 10:30:33 -0500

    Probar motor de la base

--- BrazoRobotico.ino
+++ BrazoRobotico.ino
@@ -1,10 +1,17 @@
+#include <Servo.h>
+#define motorBase 12//Declaro el pin de la base
+
+
+Servo Base;//Declaro el servomotor de la base
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  Base.attach(motorBase);//Inicializamos el servo
+  Base.write(90);
+  delay(2000);
+  Base.write(0);
+  delay(2000);
+  Base.write(180);
 }
 
 void loop() {
